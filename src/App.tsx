81d2526f57d2d50b9a0024fc2b461839363bbac3
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-23 21:48:16 +0200

    build input form and order states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Pic from "./assets/yuhos.png";
 import Calculator from "./components/Calculator";
 
 const App = () => {
+  const [deliveryCost, setDeliveryCost] = useState<number>(0);
   return (
     <div>
       <Calculator />
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
 import { ChangeEvent, FormEvent } from "react";
 import { TextField, Button, Typography, Paper } from "@mui/material";
-import { textAlign } from "@mui/system";
 
 // this is interface
 interface woltOrder {
   cartValue: number;
   deliveryDistance: number;
   numberOfItems: number;
-  OrderTime: Date;
 }
 
 const Calculator: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState<string>("");
   const [error, setError] = useState<string>("");
-  const [deliveryCost, setDeliveryCost] = useState<number>(0);
   const [order, setOrder] = useState<woltOrder>({
     cartValue: 0,
     deliveryDistance: 0,
     numberOfItems: 0,
-    OrderTime: new Date(),
   });
 
-  // setiitng the oreder state from input
-
-  const handelInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const name = e.target.name;
-    const value = e.target.value;
-
-    setOrder({ ...order, [name]: +value });
+  const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedDate(e.target.value);
+    const selectedDateObject = new Date(e.target.value);
+    const currentDate = new Date();
+    if (selectedDateObject < currentDate) {
+      setError("Selected date should be greater than or equal to today's date");
+    } else {
+      setError("");
+    }
   };
+
+  // clear
+  const clearHandeler = () => {
+    setOrder({
+      cartValue: 0,
+      deliveryDistance: 0,
+      numberOfItems: 0,
+    });
+    setError("");
+    setSelectedDate("");
+  };
+
   // input validator function
 
   const validate = (order: woltOrder) => {
@@ -48,13 +59,19 @@ const Calculator: React.FC = () => {
 
   const handleCalculateClick = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const error: string = validate(order);
-    console.log(error);
-    if (error) {
+    const error1: string = validate(order);
+
+    console.log(error.length);
+
+    if (error1 || error) {
       setError(error);
       return;
+    } else {
+      setError("");
+      clearHandeler();
+
+      console.log(order);
     }
-    setError("");
   };
 
   // render function
@@ -72,7 +89,9 @@ const Calculator: React.FC = () => {
             variant="outlined"
             fullWidth
             value={order.cartValue === 0 ? "" : order.cartValue}
-            onChange={handelInputChange}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setOrder({ ...order, cartValue: +e.target.value })
+            }
           />
         </div>
 
@@ -84,7 +103,9 @@ const Calculator: React.FC = () => {
           variant="outlined"
           fullWidth
           value={order.deliveryDistance === 0 ? "" : order.deliveryDistance}
-          onChange={handelInputChange}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setOrder({ ...order, deliveryDistance: +e.target.value })
+          }
         />
         <TextField
           type="number"
@@ -93,16 +114,18 @@ const Calculator: React.FC = () => {
           variant="outlined"
           fullWidth
           value={order.numberOfItems === 0 ? "" : order.numberOfItems}
-          onChange={handelInputChange}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setOrder({ ...order, numberOfItems: +e.target.value })
+          }
         />
         <TextField
+          required
           type="date"
           name="OrderTime"
-          label="Order Time"
           variant="outlined"
           fullWidth
-          value={order.OrderTime}
-          onChange={handelInputChange}
+          onChange={handleDateChange}
+          value={selectedDate}
         />
         <Button
           variant="contained"
@@ -121,7 +144,7 @@ const Calculator: React.FC = () => {
           size="small"
           fullWidth
           sx={{ marginBottom: "10px" }}
-          // onClick={clearHandeler}
+          onClick={clearHandeler}
         >
           {" "}
           Clear
